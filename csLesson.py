dfd60324740b0d1f4aff30eba316cadf32eff154
Bryson77 <109231608+Bryson77@users.noreply.github.com>
2026-01-22 17:45:26 +0200

    Lecture 3 of CS Class

---
+++ csLesson.py
@@ -0,0 +1,11 @@
+#verb = input("Add a verb here!")
+
+#print("I can", verb, "better than you") 
+#print(verb * 5 ) 
+
+#s = "bryson"
+#print(s[0:4:2]) 
+mySum = 0
+for i in range(10):
+ mySum +=i
+ print(mySum)
---
+++ lists.py
@@ -0,0 +1,5 @@
+names = ["Bryson", "Jack", "Bane", "Odeal"]
+
+for i in range(2, 4):
+   print(names[i])
+i = 0
